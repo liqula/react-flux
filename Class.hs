4f511d3df4d43de3c2e790a46b96c0a82354b778
John Lenz <lenz@math.uic.edu>
2015-07-24 01:36:23 -0500

    More updates

--- Class.hs
+++ Class.hs
@@ -2,37 +2,56 @@ module React.Flux.Class (
     ReactClass_
   , ReactClassRef
   , ReactClass(..)
-  , ComponentViewEventHandler
-  , mkComponentView
-  , PureClassEventHandler
-  , mkPureClass
+  , ControllerViewEventHandler
+  , mkControllerView
+  , StatefulClassEventHandler
+  , mkStatefulClass
   , ClassEventHandler
   , mkClass
-
 ) where
 
 -- | This type is for the return value of React.createClass.
 data ReactClass_
-
--- | A JSRef to a javascript React class.
 type ReactClassRef props = JSRef ReactClass_
 
--- | TODO
+-- | A React class is conceptually a (stateful) function from properties to a tree of elements.
+--
+-- This module supports 3 kinds of pureish classes and 1 unpure class.
+--
+-- * Controller View.  The controller view provides the glue code between a store and the view, and
+-- as such is a pure function taking as input the store data and the properties and producing a tree
+-- of elements.  In addition, any event handlers attached to elements can only produce actions.
+--
+-- * View.  A view is pure function from props to a tree of elements.  It is best modeled by just a
+-- Haskell function, so a 'ReactClass' is not needed.
+--
+-- * Stateful Class.  A stateful class is a class which keeps track of some internal state, but it
+-- is impossible for the state to leak outside of the class.  It consists of a pure function taking
+-- as input the properties and current state and producing a tree of elements.  Event handlers
+-- registered on elements can transform the state and produce actions, but cannot perform any other
+-- actions.
+--
+-- The above classes I call pureish because the this module enforces at the type level that no @IO@
+-- can occur either during rendering or event handlers (of course under the hood there are many
+-- side-effects).  Occasionally, you may need to create a class that performs arbitrary IO either
+-- during rendering or event handlers, and this module does provide an escape-hatch in 'mkClass'.
 newtype ReactClass props = ReactClass (ReactClassRef_ props)
 
 -- | The class render callback is a haskell function given to the javascript class
 -- that is called every time the class is to be rendered.  The argument to this callback is
--- javascript object (since there are 4 arguments and the "GHCJS.Foreign.Callbacks" module
--- only creates callbacks with up to two arguments).  The properties of this object are:
+-- javascript object used both for input and output.  The properties of this object are:
 --
 -- * state :: Export state
 -- * props :: Export props
--- * setState.  The setState function for this class.
 -- * newCallbacks :: [Callback].  This array is set by Haskell, and contains all the event
 --       callbacks created as part of the rendering.  These callbacks will be stored and
 --       after the next render, these callbacks will be freed.
 -- * elem  This is set by Haskell and contains the value that should be returned by the render
 --         function back to React.
+--
+--  In addition, for classes (not controller-views), there are two additional properties, getState
+--  and setState.  These two functions are sent from the javascript to Haskell for use inside the
+--  event handlers.
 data RenderCbArgs
 
 foriegn import javascript unsafe
@@ -43,20 +62,34 @@ foreign import javascript unsafe
     "$1.props"
     js_RenderCbRetrieveProps :: JSRef RenderCbArgs -> IO (Export props)
 
-type SetStateJSFun = JSRef (JSRef state -> IO ())
-
-foreign import javascript unsafe
-    "$1.setState"
-    js_RenderCbRetrieveSetState :: JSRef RenderCbArgs -> IO SetStateJSFun
-
-foreign import javascript unsafe
-    "$1($2)"
-    js_SetState :: SetStateJSFun -> JSRef state -> IO ()
-
 foreign import javascript unsafe
     "$1.newCallbacks = $2; $1.elem = $3;"
     js_RenderCbSetResults :: JSRef RenderCbArgs -> JSRef a -> JSRef b -> IO ()
 
+type GetStateFn = JSRef (IO (JSRef state))
+type SetStateFn = JSRef (JSRef state -> IO ())
+
+foreign import javascript unsafe
+    "$1.getState"
+    js_RenderCbRetrieveGetStateFn :: JSRef RenderCbArgs -> IO GetStateFn
+
+foreign import javascript unsafe
+    "$1.setState"
+    js_RenderCbRetrieveSetStateFn :: JSRef RenderCbArgs -> IO SetStateFn
+
+foreign import javascript unsafe
+    "$1()"
+    js_GetState :: GetStateFn -> IO (JSRef state)
+
+foreign import javascript unsafe
+    "$1($2)"
+    js_SetState :: GetStateFn -> JSRef state -> IO ()
+
+-- | Create a controller view.  On mounting and unmounting, register with the store to be notified
+-- of state changes.  Any time the properties change they must be released, so @releaseProps@
+-- is 'releaseExport' exported to javascript.  Also, the event callbacks created during a render
+-- must eventually be cleaned up.  So 'releaseCb' is 'releaseCallback' exported to javascript, and
+-- is used after rendering to clean up the callbacks from the previous render.
 foreign import javascript unsafe
     "(function(name, store, renderCb, releaseCb, releaseProps) {
         React.createClass({ \
@@ -91,26 +124,31 @@ foreign import javascript unsafe
             _currentCallbacks: [], \
         }) \
     })($1, $2, $3, $4, $5)"
-    js_createComponentView :: JSString
+    js_createControllerView :: JSString
                            -> ReactStoreRef storeData
                            -> (JSFun (JSRef RenderCbArgs -> IO ()))
                            -> (JSFun (Callback a -> IO ()))
                            -> (JSFun (Export a -> IO ())
                            -> IO (ReactClassRef props)
 
-type ComponentViewEventHandler = Event -> [SomeStoreAction]
+-- | Event handlers in a controller view transform events into actions.
+type ControllerViewEventHandler = Event -> [SomeStoreAction]
 
-expandComponentHandler :: ComponentViewEventHandler -> RawEventHandler
-expandComponentHandler handler evt = mapM_ dispatchSome $ handler evt
+-- | Transform a controller view handler to a raw handler.
+expandCtrlViewHandler :: ControllerViewEventHandler -> RawEventHandler
+expandCtrlViewHandler handler evt = mapM_ dispatchSome $ handler evt
     where
         dispatchSome (SomeStoreAction store action) = dispatch store action
 
-mkComponentView :: Typeable props
-                => String
-                -> ReactStore storeData
-                -> (storeData -> props -> ReactElement ComponentViewEventHandler)
-                -> ReactClass props
-mkComponentView name (ReactStore store) buildNode = unsafePerformIO $ do
+-- | A controller view provides the glue between a 'ReactStore' and the DOM.
+--
+-- TODO
+mkControllerView :: Typeable props
+                 => String -- ^ A name for this class
+                 -> ReactStore storeData -- ^ The store this controller view should attach to.
+                 -> (storeData -> props -> ReactElement ControllerViewEventHandler) -- ^ The rendering function
+                 -> ReactClass props
+mkControllerView name (ReactStore store) buildNode = unsafePerformIO $ do
     renderCb <- syncCallback1 ThrowWouldBlock $ \arg -> do
 
         storeDataE <- js_RenderCbRetrieveState arg
@@ -121,7 +159,7 @@ mkComponentView name (ReactStore store) buildNode = unsafePerformIO $ do
         mprops <- derefExport propsE
         props <- maybe (error "Unable to load props") return mprops
 
-        let node = fmap expandComponentHandler $ buildNode storeData props
+        let node = fmap expandCtrlViewHandler $ buildNode storeData props
         (element, evtCallbacks) <- runWriterT $ renderNode node
 
         evtCallbacksRef <- toJSArg evtCallbacks
@@ -131,8 +169,10 @@ mkComponentView name (ReactStore store) buildNode = unsafePerformIO $ do
 
     releaseProps <- syncCallback1 ThrowWouldBlock releaseExport
 
-    ReactClass <$> js_createComponentView (toJSString name) store renderCb releaseCb releaseProps
+    ReactClass <$> js_createControllerView (toJSString name) store renderCb releaseCb releaseProps
 
+-- | Create a class which tracks its own state.  Similar releasing needs to happen for callbacks and
+-- properties as for controller views.
 foreign import javascript unsafe
     "(function(name, initialState, renderCb, releaseCb, releaseProps) {
         React.createClass({ \
@@ -150,6 +190,7 @@ foreign import javascript unsafe
             render: function() { \
                 var arg = { \
                     state: this.state, \
+                    getState: function() { return this.state; }, \
                     setState: this.setState, \
                     props: this.props, \
                     newCallbacks: [], \
@@ -170,55 +211,90 @@ foreign import javascript unsafe
                    -> JSFun (Export a -> IO ())
                    -> IO (ReactClassRef props)
 
-type PureClassEventHandler state = Event -> ([SomeStoreAction], state)
+-- | A stateful class event handler transforms events into store actions and a new state.
+-- If the new state is nothing, no change is made to the state (which allows an optimization in that
+-- we do not need to re-render the view).
+type StatefulClassEventHandler state = state -> Event -> ([SomeStoreAction], Maybe state)
 
-expandPureClassHandler :: PureClassEventHandler state -> ClassEventHandler state
-expandPureClassHandler handler setState evt = do
-    let (actions, newState) <- handler evt
-    setState newState
-    let dispatchSome (SomeStoreAction store action) = dispatch store action
-    mapM_ dispatchSome actions
+-- | Transform a stateful class event handler to a raw event handler
+expandStatefulClassHandler :: (FromJSON state, ToJSON state)
+                           => GetStateFn -> SetStateFn -> StatefulClassEventHandler state -> RawEventHandler
+expandStatefulClassHandler getState setState handler evt = do
+    state <- parseState <$> js_GetState getState
+    let (actions, mNewState) = handler state evt
+    case mNewState of
+        Nothing -> return ()
+        Just newState -> do
+            newStateRef <- toJSRef_aeson newState
+            js_SetState setState newStateRef
+    mapM_ dispatchSomeAction actions
 
-mkPureClass :: (ToJSON state, FromJSON state, Typeable props)
-            => String
-            -> state
-            -> (state -> props -> ReactElement (PureClassEventHandler state))
+-- | A stateful class keeps track of internal state, but the definition of the rendering and
+-- event handlers are pure functions.
+--
+-- TODO
+-- >todoInput :: ReactClass String
+-- >todoInput = mkStatefulClass "todoInput" "" $ \state props ->
+-- >    let ct = if null state then props else state
+-- >     in el "input" [evt "onChange" (\oldState evt -> ([], oldState ++ evt)]
+mkStatefulClass :: (ToJSON state, FromJSON state, Typeable props)
+            => String -- ^ A name for this class
+            -> state -- ^ The initial state
+            -> (state -> props -> ReactElement (PureClassEventHandler state)) -- ^ The rendering function
             -> ReactClass props
-mkPureClass name initial render = mkClass name initial render'
+mkStatefulClass name initial render = mkClassHelper expandStatefulClassHandler name initial render'
     where
-        render' state props = fmap expandPureClassHandler $ render state props
+        render' state props = return $ render state props
 
-type ClassEventHandler state = (state -> IO ()) -> Event -> IO ()
+-- | A class event handler can perform arbitrary IO, producing the new state.  If the new state is
+-- nothing, no change is made to the state (which allows an optimization in that we do not need to
+-- re-render the view).
+type ClassEventHandler state = state -> Event -> IO (Maybe state)
 
-expandClassHandler :: ToJSON state => SetStateJSFun -> ClassEventHandler state -> RawEventHandler
-expandClassHandler setStateJs handler evt = do
-    let setState state = do newStateRef <- toJSRef_aeson newState
-                            js_SetState setState newStateRef
-    handler setState evt
+-- | Transform a class event handler to a raw event handler.
+expandClassHandler :: (FromJSON state, ToJSON state)
+                   => GetStateFn -> SetStateFn -> ClassEventHandler state -> RawEventHandler
+expandClassHandler getState setState handler evt = do
+    state <- parseState <$> js_GetState getState
+    mNewState <- handler state evt
+    case mNewState of
+        Nothing -> return ()
+        Just newState -> do
+            newStateRef <- toJSRef_aeson newState
+            js_SetState setState newStateRef
 
+-- | Create a class allowed to perform arbitrary IO during rendering and event handlers.
 mkClass :: (ToJSON state, FromJSON state, Typeable props)
-        => String
-        -> state
-        -> (state -> props -> ReactElement (ClassEventHandler state))
+        => String -- ^ A name for this class
+        -> state -- ^ The initial state
+        -> (state -> props -> IO (ReactElement (ClassEventHandler state))) -- ^ The rendering function
         -> ReactClass props
-mkClass name initial render = unsafePerformIO $ do
+mkClass = mkClassHelper expandClassHandler
+
+-- | Helper function used for 'mkStatefulClass' and 'mkClass'
+mkClassHelper :: (ToJSON state, FromJSON state, Typeable props)
+              => (GetStateFn -> SetStateFn -> eventHandler -> RawEventHandler))
+              -> String
+              -> state
+              -> (state -> props -> IO (ReactElement eventHandler))
+              -> ReactClass props
+mkClassHelper expandHandler name initial render = unsafePerformIO $ do
 
     initialRef <- toJSRef_aeson initial
 
     renderCb <- syncCallback1 ThrowWouldBlock $ \arg -> do
 
         stateRef <- js_RenderCbRetrieveState arg
-        mstate <- fromJSRef stateRef
-        stateVal <- maybe (error "Unable to decode class state") return mstate
-        state <- case fromJSON stateVal of
-                    Error err -> error $ "Unable to decode class state: " ++ err
-                    Success s -> return s
 
         propsE <- js_RenderCbRetrieveProps arg
         mprops <- derefExport propsE
         props <- maybe (error "Unable to load props") return mprops
 
-        let node = fmap expandClassHandler $ buildNode state props
+        getStateFn <- js_RenderCbRetrieveGetStateFn
+        setStateFn <- js_RenderCbRetrieveSetStateFn
+        let f = expandHandler getStateFn setStateFn
+        node <- fmap f <$> render state props
+
         (element, evtCallbacks) <- runWriterT $ renderNode node
 
         evtCallbacksRef <- toJSArg evtCallbacks
@@ -228,4 +304,13 @@ mkClass name initial render = unsafePerformIO $ do
 
     releaseProps <- syncCallback1 ThrowWouldBlock releaseExport
 
-    ReactClass <$> js_createComponentView (toJSString name) initialRef renderCb releaseCb releaseProps
+    ReactClass <$> js_createClass (toJSString name) initialRef renderCb releaseCb releaseProps
+
+-- | Parse the state from a JSRef.
+parseState :: FromJSON state => JSRef state -> IO state
+parseState stateRef = do
+    mstate <- fromJSRef stateRef
+    stateVal <- maybe (error "Unable to decode class state") return mstate
+    state <- case fromJSON stateVal of
+                Error err -> error $ "Unable to decode class state: " ++ err
+                Success s -> return s
--- Store.hs
+++ Store.hs
@@ -6,7 +6,7 @@ module Store (
   , SomeStoreAction(..)
   , mkStore
   , dispatch
-
+  , dispatchSomeAction
 ) where
 
 -- | As part of each store, we have a javascript object with two properties:
@@ -18,11 +18,17 @@ module Store (
 --
 -- This type is used to represent this foreign javascript object
 data ReactStore_
-
--- | A JSRef to a javascript store object.
 type ReactStoreRef storeData = JSRef ReactStore_
 
--- | TODO
+-- | A store contains application state, receives actions from the dispatcher, and notifies
+-- component views to re-render themselves.
+--
+-- A store keeps a global reference to a value of type @storeData@, which must be an instance of
+-- 'StoreData'.  When the store receives an action from 'dispatch', it first transforms the data and
+-- then notifies all component views to re-render themselves.
+-- 
+-- You can have multiple stores; it should be the case that all of the state required to render the
+-- page is contained in the stores.
 newtype ReactStore storeData = ReactStore {
     -- | A reference to the foreign javascript part of the store.
     storeRef :: ReactStoreRef storeData
@@ -35,13 +41,18 @@ newtype ReactStore storeData = ReactStore {
   , storeData :: MVar storeData
 }
 
--- | TODO
-class StoreData storeData where
+-- | All data in a store must be an instance of this typeclass.
+class Typeable storeData => StoreData storeData where
+    -- | The actions that this store accepts
     type StoreAction storeData
+
+    -- | The action that the store uses to transform the data.  Note that if this action
+    -- throws an exception, the transform will be aborted and the old store data will be kept
+    -- unchanged.
     transform :: StoreAction storeData -> storeData -> IO storeData
 
--- | TODO
-data SomeStoreAction = forall storeData. (StoreData storeData, Typeable (StoreAction storeData))
+-- | An existential type for some store action.
+data SomeStoreAction = forall storeData. StoreData storeData
     => SomeStoreAction (ReactStore storeData) (StoreAction storeData)
 
 -- | Create the javascript half of the store.
@@ -49,17 +60,18 @@ foreign import javascript unsafe
     "{sdata:$1, views: []}"
     js_CreateStore :: Export storeData -> IO (ReactStoreRef storeData)
 
--- | When updating the store data we need call 'releaseExport' on export the old store data, and we
+-- | When updating the store data we need call 'releaseExport' on the old store data, and we
 -- want this to be safe even if an exception occurs.  To do so, the update function receives a
 -- javascript object with a single property @export@.  Initially, this contains the Export for the
 -- new data.  The update first swaps this property with the current data, and then calls out to the
 -- component views to notify them of the change.  This type is for this argument object.
 data UpdateStoreArgument_
+type UpdateStoreArgument = JSRef UpdateStoreArgument_
 
 -- | Create a new argument object to pass to UpdateStore.
 foreign import javascript unsafe
     "{export:$1}"
-    js_UpdateStore_CreateArgument :: Export storeData -> IO (JSRef UpdateStoreArgument_)
+    js_UpdateStore_CreateArgument :: Export storeData -> IO UpdateStoreArgument
 
 -- | Perform the update, swapping the old export and the new export and then notifying the component
 -- views.
@@ -70,26 +82,34 @@ foreign import javascript unsafe
         arg.export = oldD; \
         store.views.map(function(f) { f(store.sdata); }; \
     })($1, $2)"
-    js_UpdateStore :: ReactStoreRef storeData
-                   -> JSRef UpdateStoreArgument_
-                   -> IO ()
+    js_UpdateStore :: ReactStoreRef storeData -> UpdateStoreArgument -> IO ()
 
 -- | Retrieve the old export so that it can be released.
 foreign import javascript unsafe
     "$1.export"
-    js_UpdateStore_RetrieveOldExport :: JSRef UpdateStoreArgument_ -> IO (Export storeData)
+    js_UpdateStore_RetrieveOldExport :: UpdateStoreArgument -> IO (Export storeData)
 
--- | TODO
-mkStore :: (StoreData storeData, Typeable storeData, Typeable (StoreAction storeData)) => storeData -> IO (ReactStore storeData)
+-- | Create a new store from some initial data.
+--
+-- For the TODO example application, we can create a single store.
+--
+-- >newtype TodoState = TodoState [Todo]
+-- >
+-- >todoStore :: ReactStore TodoState
+-- >todoStore = mkStore $ TodoState []
+mkStore :: StoreData storeData => storeData -> ReactStore storeData
 mkStore initial = unsafePerformIO $ do
     i <- export initial
     storeRef <- jsCreateStore i
     storeMVar <- newMVar initial
     return $ ReactStore storeRef storeMVar
 
--- | TODO
-dispatch :: (StoreData storeData, Typeable (StoreAction storeData))
-         => ReactStore storeData -> StoreAction storeData -> IO ()
+-- | Dispatch an action to a store.  This first causes the store data to transform according to the
+-- action and then notifies all component views that the store data has changed.
+--
+-- This function uses an MVar to make sure only a single thread is updating the store and
+-- re-rendering the view at a single time.  Thus this function can block.
+dispatch :: StoreData storeData => ReactStore storeData -> StoreAction storeData -> IO ()
 dispatch store action = modifyMVar_ (storeData store) $ \oldData -> do
 
     newData <- transform action oldData
@@ -101,3 +121,7 @@ dispatch store action = modifyMVar_ (storeData store) $ \oldData -> do
         `finally` js_UpdateStore_RetrieveOldState arg >>= releaseExport
 
     return newData
+
+-- | Dispatch some store action.
+dispatchSomeAction :: SomeStoreAction -> IO ()
+dispatchSomeAction (SomeStoreAction store action) = dispatch store action
