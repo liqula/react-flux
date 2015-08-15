c7534ba1141a306076fe48a33969840269d831c4
John Lenz <lenz@math.uic.edu>
2015-08-14 23:55:14 -0500

    Add Addons

---
+++ src/React/Flux/Addons.hs
@@ -0,0 +1,90 @@
+-- | Bindings for the <https://facebook.github.io/react/docs/addons.html React addons> that make
+-- sense to use from Haskell.  At the moment, that is only the
+-- <https://facebook.github.io/react/docs/animation.html animation> and
+-- <https://facebook.github.io/react/docs/perf.html performance tools>.
+module React.Flux.Addons (
+  -- * Animation
+    cssTransitionGroup
+  , CSSTransitionProps(..)
+  , defaultTransitionProps
+
+  --- * Perf
+  , perfStart
+  , perfStop
+  , perfPrintInclusive
+  , perfPrintExclusive
+  , perfPrintWasted
+  , perfPrintDOM
+) where
+
+import Data.Typeable (Typeable)
+
+#ifdef __GHCJS__
+import GHCJS.Types (JSRef)
+#else
+type JSRef a = ()
+#endif
+
+import React.Flux.Internal
+import React.Flux.Views
+import React.Flux.PropertiesAndEvents
+
+foreign import javascript unsafe
+    "React['addons']['CSSTransitionGroup']"
+    js_CSSTransitionGroup :: JSRef ()
+
+-- | The properties for the CSS Transition Group.
+data CSSTransitionProps = CSSTransitionProps
+    { transitionName :: String
+    , transitionAppear :: Bool
+    , transitionEnter :: Bool
+    , transitionLeave :: Bool
+    } deriving (Typeable)
+
+-- | Default properties for CSS Transition Group, using \"react-transition\" as the transition name.
+defaultTransitionProps :: CSSTransitionProps
+defaultTransitionProps = CSSTransitionProps
+    { transitionName = "react-transition"
+    , transitionAppear = False
+    , transitionEnter = True
+    , transitionLeave = True
+    }
+
+-- | The <https://facebook.github.io/react/docs/animation.html ReactCSSTransitionGroup> element.  At
+-- the moment, only the high-level API is supported.
+cssTransitionGroup :: CSSTransitionProps -> ReactElementM eventHandler a -> ReactElementM eventHandler a
+cssTransitionGroup p children = foreignClass js_CSSTransitionGroup props children
+    where
+        props = [ "transitionName" @= transitionName p
+                , "transitionAppear" @= transitionAppear p
+                , "transitionEnter" @= transitionEnter p
+                , "transitionLeave" @= transitionLeave p
+                ]
+
+--------------------------------------------------------------------------------
+-- Perf
+--------------------------------------------------------------------------------
+
+foreign import javascript unsafe
+    "React['addons']['Perf']['start']()"
+    perfStart :: IO ()
+
+foreign import javascript unsafe
+    "React['addons']['Perf']['stop']()"
+    perfStop :: IO ()
+
+foreign import javascript unsafe
+    "React['addons']['Perf']['printInclusive']()"
+    perfPrintInclusive :: IO ()
+
+foreign import javascript unsafe
+    "React['addons']['Perf']['printExclusive']()"
+    perfPrintExclusive :: IO ()
+
+foreign import javascript unsafe
+    "React['addons']['Perf']['printWasted']()"
+    perfPrintWasted :: IO ()
+
+foreign import javascript unsafe
+    "React['addons']['Perf']['printDOM']()"
+    perfPrintDOM :: IO ()
